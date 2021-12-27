222a112e28d81582ff29d4ffb22e3fd07d582368
Emre Akcadag <akcadagemre@gmail.com>
2021-12-27 16:35:36 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Emre Akçadağ on 27.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
